4c83d6f63ab1d6d3695afc3cdc2c2d3a8376c345
Ghis008 <73109361+Ghis008@users.noreply.github.com>
2024-04-29 07:57:51 -0400

    Redesigned the entire website

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+
 const getElement = (selector) => {
     const element = document.querySelector(selector)
 
@@ -9,4 +10,29 @@ const getElement = (selector) => {
 
 const date = getElement('#date')
 const currentYear = new Date().getFullYear()
-date.textContent = currentYear
+date.textContent = currentYear;
+
+
+const toggleMenu = () => {
+
+    const menu = document.querySelector('.menu-links');
+    const icon = document.querySelector('.hamburger-icon');
+
+    menu.classList.toggle('open');
+    icon.classList.toggle('open');
+}
+
+
+/* // Change the text of all headings to "Gautier was here!"
+const headings = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
+for (let i = 0; i < headings.length; i++) {
+    headings[i].textContent = 'Gautier was here!';
+}
+
+// Display an alert message when the page loads
+window.onload = function () {
+    alert('Hey Gautier, welcome to my website!'); // Replace with any message you like
+} */
+
+// document.body.style.transform = "scaleX(-1)";
+
